a734a36ba8fa612fd0bdbe91561d69308d8cfee3
qazsdikj <65976447+qazsdikj@users.noreply.github.com>
2023-03-20 23:10:47 +0900

    Create CMSmenu.java

---
+++ CMSmenu.java
@@ -0,0 +1,9 @@
+public class CMSmenu {
+	public static void main(String[] args) {
+		 System.out.println("차량 추가");
+		System.out.println("차량 제거");
+		System.out.println("차량 수정");
+		System.out.println("차량 정보");
+		System.out.println("나가기");
+	}
+}
