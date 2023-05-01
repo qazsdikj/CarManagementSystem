33b792b39d7a7f62147b6d74962d234950c522ca
qazsdikj <65976447+qazsdikj@users.noreply.github.com>
2023-05-01 22:51:20 +0900

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,9 +1,15 @@
+package Car;
+
+import java.util.Scanner;
+
 public class Car {
-	String CarNum;
-	String CarType;
-	String CarMaker;
-	String CarName;
-	int CarAge;
+
+	protected CarKind kind = CarKind.PASSENGER;
+	protected String CarNum;
+	protected String CarType;
+	protected String CarMaker;
+	protected String CarName;
+	protected int CarAge;
 	
 	public Car() {
 	}
@@ -11,10 +17,10 @@ public class Car {
 	
 	public Car(String CarName, String CarNum) {
 		this.CarName = CarName;
-		this.CarNum = CarNum;
+		this.CarNum = CarNum; 
 	}
 	
-	public Car(String CarName, String CarNum, String CarType, String CarMaker, int CarAge) {
+	public Car(String CarNum, String CarType, String CarMaker, String CarName, int CarAge) {
 		this.CarName = CarName;
 		this.CarNum = CarNum;
 		this.CarType = CarType;
@@ -22,48 +28,91 @@ public class Car {
 		this.CarAge = CarAge;
 	}
 	
+	public CarKind getKind() {
+		return kind;
+	}
+
+
+	public void setKind(CarKind kind) {
+		this.kind = kind;
+	}
+
+
+	public String getCarNum() {
+		return CarNum;
+	}
+
+
+	public void setCarNum(String carNum) {
+		CarNum = carNum;
+	}
+
+
+	public String getCarType() {
+		return CarType;
+	}
+
+
+	public void setCarType(String carType) {
+		CarType = carType;
+	}
+
+
+	public String getCarMaker() {
+		return CarMaker;
+	}
+
+
+	public void setCarMaker(String carMaker) {
+		CarMaker = carMaker;
+	}
+
+
+	public String getCarName() {
+		return CarName;
+	}
+
+
+	public void setCarName(String carName) {
+		CarName = carName;
+	}
+
+
+	public int getCarAge() {
+		return CarAge;
+	}
+
+
+	public void setCarAge(int carAge) {
+		CarAge = carAge;
+	}
+
 	public void PrintInfo() {
 		System.out.println("차번 : " + CarNum + " 차종 : " + CarType + " 제조사 : " + CarMaker + " 차명 : " + CarName + " 연식 : " + CarAge);
 
 	}
-	public void PrintCarNum() {
-		System.out.println(CarNum);
-	}
 	
-	public void PrintCarType() {
-		System.out.println(CarType);
-	}
-	
-	public void PrintCarMaker() {
-		System.out.println(CarMaker);
-	}
-	
-	public void PrintCarName() {
-		System.out.println(CarName);
-	}
-	
-	public void PrintCarAge() {
-		System.out.println(CarAge);
-	}
-	
-	public void SetCarNum(String CarNum) {
-		this.CarNum = CarNum; 
-	}
-	
-	public void SetCarType(String CarType) {
-		this.CarType = CarType;
-	}
-	
-	public void SetCarMaker(String CarMaker) {
-		this.CarMaker = CarMaker;
-	}
-	
-	public void SetCarName(String CarName) {
-		this.CarName = CarName;
-	}
-	
-	public void SetCarAge(int CarAge) {
-		this.CarAge = CarAge;
+	public void getUserInput(Scanner input) {
+		System.out.print("차량 번호 입력 : ");
+		String CarNum = input.next();
+		this.setCarNum(CarNum);
+		
+		System.out.print("차종 입력 : ");
+		String CarType = input.next();
+		this.setCarType(CarType);
+		
+		System.out.print("제조사 입력 : ");
+		String CarMaker = input.next();
+		this.setCarMaker(CarMaker);
+		
+		System.out.print("차명 입력 : ");
+		String CarName = input.next();
+		this.setCarName(CarName);
+		
+		System.out.print("연식 입력 : ");
+		int CarAge = input.nextInt();
+		this.setCarAge(CarAge);
+		
 	}
 
 }
